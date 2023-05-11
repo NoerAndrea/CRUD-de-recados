9e60c4d053e5d9ef570ce11e050272fc3de8a2fa
NoerAndrea <116918551+NoerAndrea@users.noreply.github.com>
2023-05-11 17:01:50 -0300

    Criptografar senha e validar usuário
    Nesta etapa tornei a senha mais segura tornando uma criptografia, após validei usuário para logar com nome e senha.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,67 @@
 import express, {request, response} from "express";
+const bcrypt = require('bcrypt');
 
 const app = express();
 
 app.use(express.json());
 
+//POST criando usuarios
+let usuarios = []
 
-//Criando POST usuarios
-let usuarios = [];
+//POST - cadastro de usuário
+app.post('/usuarios', async(request,response)=>{
+    const usuario = request.body;    
+    
+    let usuarioFiltrado = usuarios.find(user=>user.email === usuario.email);
 
-app.post('/usuarios', (request,response)=>{
-    const usuario = request.body;
-    usuarios.push(usuario);
-    console.log(usuario);
+    if(usuarioFiltrado){
+        return response.status(409).send("Usuário já existe.")
+    }
+    
+    //Criptografar senha
+    let senhaCriptografada = await bcrypt.hash(usuario.senha, 10)
 
-    return response.status(204).json(usuarios[usuarios.length -1]);
+    const novoUsuario = {
+        id: usuarios.length + 1,
+        nome: usuario.nome,
+        email: usuario.email,
+        senha:senhaCriptografada
+    }
+
+    usuarios.push(novoUsuario);
+    console.log(usuario)
+    return response.status(201).send("Usuário criado com sucesso!"); 
 });
 
+// GET - traz detalhes (lista ou um item por vez
 app.get("/usuarios", (request, response)=>{
     return response.status(201).json(usuarios);
 })
+// POST - Ususario já existente find nome e senha já estão criados no array bycript.comper
+
+app.post('/usuarios/login', (request,response)=>{
+    const filtroLogin = request.body;   
+
+    const usuario = usuarios.find(usuario=>usuario.nome===filtroLogin.nome);
+
+    if(!usuario){
+        return response.status(402).json("Por favor, digite um usuário já cadastrado.")
+    }  
+
+    bcrypt.compare(filtroLogin.senha, usuario.senha, function(err, result){
+        if(result){
+            return response.status(200).json("Usuário logado.");
+        }else{
+            return response.status(402).json("Usuário inválido.")
+        }
+    });
+})
+
+//POST - recado
+
+// PUT - edição de 1 usuário
+
+// DELETE - exclusão de um usuário
 
 app.listen(5500, ()=>{
     console.log("Rodando")
